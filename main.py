03ef1283eaf21c6ba85083ec583a7c8378e08c95
MedicalInventoryHUNCH <bernardo.rhys.hunch@gmail.com>
2024-12-04 12:23:52 -0500

    mongodb edit file

---
+++ main.py
@@ -0,0 +1,37 @@
+
+import ndef
+import nfc
+from ndef import TextRecord
+import pymongo
+from pymongo import MongoClient
+
+
+clf = nfc.ContactlessFrontend('usb')
+
+cluster = MongoClient("mongodb+srv://bernardorhyshunch:TakingInventoryIsFun@cluster0.jpb6w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+db = cluster["Inventory"]
+collection = db["Inventory"]
+
+def idnumber():
+    if "NFCNASAMED" in str(tag_data):
+        print("scanned" + str(tag_data))
+        meds = str(tag_data)
+        splitmeds = meds.split('%')
+        print(int(splitmeds[2]))
+        intmeds = int(splitmeds[2])
+        return intmeds
+
+while True:
+
+    tag = clf.connect(rdwr={'on-connect': lambda tag: False})
+    tag_data = tag.ndef.records
+
+    if idnumber() == 2:
+
+        collection.update_many({"_id":11010111}, {"$inc":{"Amount":-1}})
+
+        break
+
+    else:
+        print("unknown tag1")
+
