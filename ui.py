2a140bf919934378dfd9d5d30619886b1416c09d
Benrardo <bernardo.rhys.hunch@gmail.com>
2025-01-14 09:37:38 -0500

    made it edit mongodb correctly based on ID (review program bernardo)

--- ui.py
+++ ui.py
@@ -4,6 +4,11 @@ from pymongo import MongoClient
 
 cluster = MongoClient("mongodb+srv://bernardorhyshunch:TakingInventoryIsFun@cluster0.jpb6w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
+db = cluster["Inventory"]
+collection = db["Inventory"]
+
+doc1 = {"_id":5, "Item":"Morphine", "Amount":56} #use this to put new docs into DB
+
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
 root = customtkinter.CTk()
@@ -11,19 +16,36 @@ root.title('Inventory taking bleh bleh bleh')
 root.geometry("800x600")
 
 def Edit():
-    print("work")
+    print('whatever')
+    return
+
+def Logs():
+    print('whatever2')
+    return
+
+LogsButton = customtkinter.CTkButton(root,
+                                     text="Logs",
+                                     command=Logs(),
+                                     width=300,
+                                     height=100,
+                                     text_color="White",
+                                     )
+LogsButton.grid(row=0, column=0)
 
 EditButton = customtkinter.CTkButton(root,
                                       text="Edit",
                                       command=Edit(),
-                                      width=298,
+                                      width=300,
                                       height=100,
                                       text_color="White",
                                       )
 
 
 
-EditButton.grid(row=0, column=0)
+EditButton.grid(row=1, column=0)
+
+
 EditButton.pack(pady=180)
+LogsButton.pack(pady=180)
 
 root.mainloop()
