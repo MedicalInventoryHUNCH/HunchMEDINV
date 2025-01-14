34fdb92fa812bd5928f4f76045ae5a85a53e80be
MedicalInventoryHUNCH <bernardo.rhys.hunch@gmail.com>
2025-01-14 10:58:41 -0500

    manual edit finished (MAKE THE GUI)

--- MONGODBEDIT.py
+++ MONGODBEDIT.py
@@ -4,7 +4,7 @@ from ndef import TextRecord
 import pymongo
 from pymongo import MongoClient
 
-choice = input("1 for new 2 for edit")
+choice = input("1 for new 2 for edit ")
 
 cluster = MongoClient("mongodb+srv://bernardorhyshunch:TakingInventoryIsFun@cluster0.jpb6w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 db = cluster["Inventory"]
@@ -12,13 +12,22 @@ collection = db["Inventory"]
 while True:
 
      if choice == "1":
-        id = input("input id number:")
-        name = input("name of medication:")
-        amount = input("input amount:")
+        id = input("input id number: ")
+        name = input("name of medication: ")
+        amount = input("input amount: ")
 
-        doc1 = {"_id":int(id), "Item":name, "Amount":int(amount)} #use this to put new docs into DB
+        doc1 = {"_id":int(id), "Item":name,
+                "Amount":int(amount)} #use this to put new docs into DB
         collection.insert_one(doc1)
         break
 
      if choice == "2":
             # if sigma then Skibidi, else Beta
+        id2 = input("input id number: ")
+        num2 = input("enter amount: ")
+        collection.update_many({"_id": int(id2)}, {"$set": {"Amount": int(num2)}})
+        break
+
+
+
+print("Updated Successfully :D")
