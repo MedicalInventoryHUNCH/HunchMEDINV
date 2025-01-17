4918bb1e493f16c9cecf52c8bde34bc16ed49a09
hunch_RB <bernardo.rhys.hunch@gmail.com>
2025-01-17 07:40:38 -0500

    added face recognition file

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-
+import time
 import ndef
 import nfc
 from ndef import TextRecord
 import pymongo
 from pymongo import MongoClient
 
-
+#MANGO MANGO MANGO MANGO
 clf = nfc.ContactlessFrontend('usb')
 
 cluster = MongoClient("mongodb+srv://bernardorhyshunch:TakingInventoryIsFun@cluster0.jpb6w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
@@ -18,10 +18,12 @@ def idnumber():
         meds = str(tag_data)
         splitmeds = meds.split('%')
         print(int(splitmeds[2]))
+
         intmeds = int(splitmeds[2])
         return intmeds
     else:
         print("med unknown tag")
+
 
 while True:
 
@@ -31,6 +33,6 @@ while True:
     idnumber()
 
     collection.update_many({"_id":idnumber()}, {"$inc":{"Amount":-1}})
-    break
+    time.sleep(2)
 
-
+    print("ready")
