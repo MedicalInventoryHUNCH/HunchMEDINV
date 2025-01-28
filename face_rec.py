8f21ec1161771d2a516610762a34608b1e4e9e9d
hunch_RB <bernardo.rhys.hunch@gmail.com>
2025-01-28 08:21:42 -0500

    small changes (6700054 lines changed)

--- face_rec.py
+++ face_rec.py
@@ -1,46 +1,58 @@
 import face_recognition
 import cv2
 import time
+import ndef
+import nfc
+from ndef import TextRecord
+import pymongo
+from pymongo import MongoClient
 
 cap = cv2.VideoCapture(0)
+
+imgencode = imgencode2 = imgencode3 = imgencode4 = imgencode5 = imgencode6 = imgencode7 = None
+
+img2 = face_recognition.load_image_file("pictures/face2.jpg")
+img3 = face_recognition.load_image_file("pictures/face3.jpg")
+img4 = face_recognition.load_image_file("pictures/face4.jpg")
+img5 = face_recognition.load_image_file("pictures/face5.jpg")
+img6 = face_recognition.load_image_file("pictures/face6.jpg")
+img7 = face_recognition.load_image_file("pictures/face7.jpg")
+
 
 while True:
     ret, frame = cap.read()
     if not ret:
         exit(":(")
 
-    #img = face_recognition.load_image_file(frame)
-    imgencode = face_recognition.face_encodings(frame)[0]
+    try:
+        imgencode = face_recognition.face_encodings(frame)[0]
 
-    img2 = face_recognition.load_image_file("pictures/face2.jpg")
-    imgencode2 = face_recognition.face_encodings(img2)[0]
+        imgencode2 = face_recognition.face_encodings(img2)[0]
 
-    img3 = face_recognition.load_image_file("pictures/face3.jpg")
-    imgencode3 = face_recognition.face_encodings(img3)[0]
+        imgencode3 = face_recognition.face_encodings(img3)[0]
 
-    img4 = face_recognition.load_image_file("pictures/face4.jpg")
-    imgencode4 = face_recognition.face_encodings(img4)[0]
+        imgencode4 = face_recognition.face_encodings(img4)[0]
 
-    img5 = face_recognition.load_image_file("pictures/face5.jpg")
-    imgencode5 = face_recognition.face_encodings(img5)[0]
+        imgencode5 = face_recognition.face_encodings(img5)[0]
 
-    img6 = face_recognition.load_image_file("pictures/face6.jpg")
-    imgencode6 = face_recognition.face_encodings(img6)[0]
+        imgencode6 = face_recognition.face_encodings(img6)[0]
 
-    img7 = face_recognition.load_image_file("pictures/face7.jpg")
-    imgencode7 = face_recognition.face_encodings(img7)[0]
+        imgencode7 = face_recognition.face_encodings(img7)[0]
+    except IndexError:
+        exit("No face detected.")
 
     known_faces = [
         imgencode2, imgencode3, imgencode4, imgencode5, imgencode6, imgencode7
     ]
+    results = face_recognition.compare_faces(known_faces, imgencode)
+    break
 
-    if len(imgencode) > 0 and len(imgencode2) > 0:
-        results = face_recognition.compare_faces(known_faces, imgencode)
-        break
-    else:
-        pass
 print(results)
 
-truthy_indices = [index for index, value in enumerate(results) if bool(value)]
+true_stuff = [index for index, value in enumerate(results) if bool(value)]
 
-print(truthy_indices)
+print(true_stuff)
+
+
+
+
